5cdf59f2b332ba37b67cc1591fa31e9a23e5cc56
starteruel <starteruel@gmail.com>
2024-10-29 11:16:58 -0300

    Repository

---
+++ src/main/java/br/fatec/product/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package br.fatec.product.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.fatec.product.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+    
+
+    
+}
